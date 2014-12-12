66c730df47df615d2ff7b2c681f4f152e7a8557e
maduraisamy <maduraisamy@paypal.com>
2014-12-11 21:37:07 -0500

    push

--- src/main/java/com/pinpal/page/PinItPage.java
+++ src/main/java/com/pinpal/page/PinItPage.java
@@ -49,33 +49,40 @@ public class PinItPage extends HeaderFooter implements Renderable
                 .div(class_("image-container"))
                 .img(class_("photo-image").id("blah").src(_projectService.getImage()).alt(""))
                 ._div()
-                .div(id("share"))
-                .input(type("checkbox").class_("check-box").id("pintrestMyPage").value(""))
-                .write("Post to My Pintrest")
-                .br().br()
-                ._div()
-                .div(id("share"))
-                .input(type("checkbox").class_("check-box").id("paypalpintrest").value("").checked("checked"))
-                .write("Post to PayPal Pintrest")
-                .br().br()
-                ._div()
 
-                .div(id("share"))
-                .input(type("checkbox").class_("check-box").id("pintrestMyPage").value("").checked("checked"))
-                .write("Post to Paypal Deals")
-                .br().br()
-                ._div()
+
+//                .div(id("share"))
+//                .input(type("checkbox").class_("check-box").id("pintrestMyPage").value(""))
+//                .write("Post to My Pintrest")
+//                .br().br()
+//                ._div()
+//                .div(id("share"))
+//                .input(type("checkbox").class_("check-box").id("paypalpintrest").value("").checked("checked"))
+//                .write("Post to PayPal Pintrest")
+//                .br().br()
+//                ._div()
+//
+//                .div(id("share"))
+//                .input(type("checkbox").class_("check-box").id("pintrestMyPage").value("").checked("checked"))
+//                .write("Post to Paypal Deals")
+//                .br().br()
+//                ._div()
 
 
                 .div(class_("pin-it-btn-div"))
                 .a(href("//www.pinterest.com/pin/create/button/?url=https%3A%2F%2Fstage2md053.qa.paypal.com%2Fdeals%2F%23%2Foffers%2Fdetails%2F947eccb0-63ac-0132-d39f-7a163e6a7efb&media=http%3A%2F%2Fi59.tinypic.com%2F2q3rzvk.jpg&description=" + _projectService
                         .getDescription()))
-                .img(src("//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_28.png"))._a()
+                .img(src("//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_28.png"))._a().br().br()
+
+                .img(src("http://s7.postimg.org/re9es4dxj/1688856378692136946.png").alt("Smiley face").width("40").height("40")).br().br()
+
+                .img(src("http://s22.postimg.org/eeraf6f19/5736343191104321411.png").alt("Smiley face").width("30").height("30")).br().br()
+
+                .img(src("  http://s4.postimg.org/d7jkle13d/instagram_icon_128_1826619604.png").alt("Smiley face").width("40").height("40"))
 
 
                 ._div()
 
-//                .a(href("/offer/").id("continue-pin").class_("continue-back-pin").style("float:left")).content("Back")
                 .a(href("/offer/").id("back")).content("Back")
 
 
